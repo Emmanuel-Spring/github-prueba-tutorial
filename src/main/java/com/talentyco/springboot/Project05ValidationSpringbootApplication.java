044e1cc1db430e0dabf41cdcbbbc4454982beb1e
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-10-22 04:21:18 -0300

    Project Config

---
+++ src/main/java/com/talentyco/springboot/Project05ValidationSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.talentyco.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project05ValidationSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project05ValidationSpringbootApplication.class, args);
+	}
+
+}
